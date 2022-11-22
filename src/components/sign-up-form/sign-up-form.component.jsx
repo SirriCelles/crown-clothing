87e7967ded4814c90742c84a9c9a2252a15e581b
SirriCelles <42035795+SirriCelles@users.noreply.github.com>
2022-11-22 01:42:50 +0100

    add signup styles, create form-input component, create button component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,15 @@
 import { useState } from "react";
-import { createAuthUserWithEmailAndPAssword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+
+import FormInput from "../form-input/form-input.component";
+
+import Button from "../button/button.component";
+
+import {
+  createAuthUserWithEmailAndPAssword,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
+import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
   displayName: "",
@@ -12,71 +22,68 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
-  console.log(formFields);
-
   const resetForm = () => {
     setFormFields(defaultFormFields);
-  }
+  };
 
   const handleChange = (event) => {
     const { name, value } = event.target;
 
-    setFormFields({...formFields, [name]: value });
+    setFormFields({ ...formFields, [name]: value });
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (password !== confirmPassword) {
-        alert('Passord mismatch');
-        return;
+      alert("Passord mismatch");
+      return;
     }
-
 
     try {
-        const { user } = await createAuthUserWithEmailAndPAssword(email, password); 
-        
-        await createUserDocumentFromAuth(user, {displayName: displayName});
+      const { user } = await createAuthUserWithEmailAndPAssword(
+        email,
+        password
+      );
 
-        resetForm();
+      await createUserDocumentFromAuth(user, { displayName: displayName });
 
+      resetForm();
     } catch (error) {
-        if (error.code === 'auth/email-already-in-use') {
-            alert('Cannot create user. Email already in use');
-        } else if (error.code === 'auth/weak-password') {
-            alert('Password should be at least 6 characters');
-        }
-        else {
-            console.error('Sign Up Error: ', error);
-        }   
+      if (error.code === "auth/email-already-in-use") {
+        alert("Cannot create user. Email already in use");
+      } else if (error.code === "auth/weak-password") {
+        alert("Password should be at least 6 characters");
+      } else {
+        console.error("Sign Up Error: ", error);
+      }
     }
-
-  }
+  };
 
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className="sign-up-container">
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
-        <label>Display Name</label>
-        <input
+        <FormInput
+          label="Display Name"
           type="text"
           required
           onChange={handleChange}
           name="displayName"
           value={displayName}
-
         />
 
-        <label>Email</label>
-        <input 
-            type="email"  
-            required 
-            onChange={handleChange} 
-            name="email" 
-            value={email}
+        <FormInput
+          label="Email"
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
         />
 
-        <label>Password</label>
-        <input
+        <FormInput
+          label="Password"
           type="password"
           required
           onChange={handleChange}
@@ -85,8 +92,8 @@ const SignUpForm = () => {
           minLength="6"
         />
 
-        <label>Confirm Password</label>
-        <input
+        <FormInput
+          label="Confirm Password"
           type="password"
           required
           onChange={handleChange}
@@ -95,7 +102,7 @@ const SignUpForm = () => {
           minLength="6"
         />
 
-        <button type="submit">Sign Up</button>
+        <Button  type="submit">Sign Up</Button>
       </form>
     </div>
   );
