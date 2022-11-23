02c1f1782de444c45dd00f4ac117f21f6fdd4988
SirriCelles <sirricelles@gmail.com>
2022-11-23 16:50:17 +0100

    completing toggle functionality

--- src/contexts/cart-context.jsx
+++ src/contexts/cart-context.jsx
@@ -1,12 +1,32 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+    // if(!cartItems || !productToAdd) return;
+    
+    // const newCartItems = [...cartItems];
+    // const index = newCartItems.findIndex(el => el.id === productToAdd.id);
+    // if (index) {
+    //     const initQnty = newCartItems[index].quantity;
+    //     initQnty += 1;
+    //     newCartItems[index].
+    // }
+   
+}
+
 export const CartContext = createContext({
     isCartOpen:false,
-    setIsCartOpen: () => {}
+    setIsCartOpen: () => {},
+    cartItems: [],
+    addItemToCart: () => {}
 });
 
 export const CartProvider = ({ children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
+    const [cartItems, setCartItems] = useState([]);
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(cartItems, productToAdd);
+    }
 
     const value = { isCartOpen, setIsCartOpen };
 
