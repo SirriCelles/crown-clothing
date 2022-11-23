b80a7ff899a9aa04d9ca21d9dffe1479e0abbe56
SirriCelles <sirricelles@gmail.com>
2022-11-23 12:40:50 +0100

    add product component

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,7 +1,20 @@
-import './product-card.styles.scss'
+import './product-card.styles.scss';
+import Button from '../button/button.component';
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
+    const { id, name, price, imageUrl } = product;
 
+    return (
+        <div className='product-card-container'>
+            <img src={imageUrl} alt={`${name} product`} />
+            <div className="footer">
+                <span className="name">{name}</span>
+                <span className="price">{price}</span>
+            </div>
+
+            <Button buttonType='inverted'>Add to Card</Button>
+        </div>
+    )
 }
 
 export default ProductCard;
