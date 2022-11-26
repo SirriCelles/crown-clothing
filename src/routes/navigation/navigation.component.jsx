063fd3840d5f03c1fd0a61def1aa535cfb3de346
SirriCelles <42035795+SirriCelles@users.noreply.github.com>
2022-11-26 03:22:55 +0100

    add styled-component package

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -13,7 +13,7 @@ import { UserContext } from "../../contexts/user.context";
 import { CartContext } from "../../contexts/cart-context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
-import "./navigation.styles.scss";
+import  {NavigationContainer, NavLinks, NavLink, LogoContainer } from "./navigation.styles";
 
 const Navigation = () => {
   const { currentUser} = useContext(UserContext);
@@ -23,28 +23,28 @@ const Navigation = () => {
 
   return (
     <Fragment>
-      <div className="navigation">
-        <Link className="logo-container" to="/">
+      <NavigationContainer>
+        <LogoContainer to="/">
           <CrwnLogo className="logo"></CrwnLogo>
-        </Link>
-        <div className="nav-links-container">
-          <Link className="nav-link" to="/shop">
+        </LogoContainer>
+        <NavLinks>
+          <NavLink to="/shop">
             SHOP
-          </Link>
+          </NavLink>
           {currentUser ? (
-            <span className="nav-link" onClick={ signOutUser }>SIGN OUT</span>
+            <NavLink as='span' onClick={ signOutUser }>SIGN OUT</NavLink>
           ) : (
-            <Link className="nav-link" to="/auth">
+            <NavLink to="/auth">
               SIGN IN
-            </Link>
+            </NavLink>
           )}
         {/* Cart Link */}
             <CartIcon />
-        </div>
+        </NavLinks>
 
         { isCartOpen && <CartDropdown /> }
         {/* Link behaves like an anchor tag */}
-      </div>
+      </NavigationContainer>
       <Outlet></Outlet>
     </Fragment>
   );
